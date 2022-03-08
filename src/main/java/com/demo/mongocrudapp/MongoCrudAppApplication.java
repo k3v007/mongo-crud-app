80346de5ce6ca7502d0d9a237f8e388bb87985c7
vivekSinha-OYO <vivek.sinha3@oyorooms.com>
2022-03-08 13:00:06 +0530

    project setup

---
+++ src/main/java/com/demo/mongocrudapp/MongoCrudAppApplication.java
@@ -0,0 +1,15 @@
+package com.demo.mongocrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Vivek Kumar Sinha
+ */
+@SpringBootApplication
+public class MongoCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoCrudAppApplication.class, args);
+    }
+}
