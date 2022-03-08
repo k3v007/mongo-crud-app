f779ff55d7f7352f7361fabe253d80bf886c5bd5
vivekSinha-OYO <vivek.sinha3@oyorooms.com>
2022-03-08 13:00:24 +0530

    employee document

---
+++ src/main/java/com/demo/mongocrudapp/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.demo.mongocrudapp.entity;
+
+import com.demo.mongocrudapp.entity.base.BaseDocument;
+import com.demo.mongocrudapp.entity.json.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDate;
+
+/**
+ * @author Vivek Kumar Sinha
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "employees")
+public class Employee extends BaseDocument {
+
+    @Field("name")
+    private String name;
+
+    @Field("employee_code")
+    private String employeeCode;
+
+    @Field("date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Field("address")
+    private Address address;
+}
